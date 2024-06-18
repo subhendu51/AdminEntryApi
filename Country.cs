940dfef63ff03f21b85ada1775fe083e9540dbe4
subhendu51 <158458243+subhendu51@users.noreply.github.com>
2024-06-18 07:29:03 +0530

    Add files via upload

---
+++ Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdminEntryApi.Models
+{
+    public partial class Country
+    {
+        public int Countryid { get; set; }
+        public string CountryName { get; set; } = null!;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public DateTime? DateDeleted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ MedicineContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AdminEntryApi.Models
+{
+    public partial class MedicineContext : DbContext
+    {
+        public MedicineContext()
+        {
+        }
+
+        public MedicineContext(DbContextOptions<MedicineContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Country> Countries { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.ToTable("Country");
+
+                entity.Property(e => e.CountryName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DateCreated).HasColumnType("datetime");
+
+                entity.Property(e => e.DateDeleted).HasColumnType("datetime");
+
+                entity.Property(e => e.DateUpdated).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
